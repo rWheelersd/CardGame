3aa64a6031949c46d9ab883c88141c35e6d5cc4d
Ryan <B9MBjDSXtnldpR3QvzuO9tVu@protonmail.ch>
2024-03-28 23:42:45 -0500

    Added a little bit of front end communication to visualize game manager work better.

---
+++ CardGame.API/Hubs/BlackjackHub.cs
@@ -0,0 +1,19 @@
+﻿using CardGame.BL.Blackjack;
+
+namespace CardGame.API.Hubs
+{
+    public class BlackjackHub
+    {
+        private readonly BlackjackManager _gamesManager;
+
+        public BlackjackHub(BlackjackManager gamesManager)
+        {
+            _gamesManager = gamesManager;
+        }
+
+        public async Task PlaceBet(Guid gameId, Guid playerId, int betAmount)
+        {
+            await _gamesManager.PlaceBet(gameId, playerId, betAmount);
+        }
+    }
+}
---
+++ CardGame.BL/Blackjack/BlackjackManager.cs
@@ -0,0 +1,69 @@
+﻿using CardGame.BL.BlackJack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGame.BL.Blackjack
+{
+    public class BlackjackManager
+    {
+        Dictionary<Guid, BlackjackGameManager> Games;
+
+        public BlackjackManager()
+        {
+            Games = new Dictionary<Guid, BlackjackGameManager>();
+        }
+
+        public Guid CreateGame()
+        {
+            try
+            {
+                Guid gameId = Guid.NewGuid();
+                Games.Add(gameId, new BlackjackGameManager());
+                return gameId;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public List<Guid> GetOpenGames()
+        {
+            try
+            {
+                return new List<Guid>();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public bool JoinGame(Guid gameId, Guid playerId)
+        {
+            try
+            {
+                if (Games.ContainsKey(gameId))
+                {
+                    
+                }
+                return false;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task PlaceBet(Guid gameId, Guid playerId, int betAmount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
