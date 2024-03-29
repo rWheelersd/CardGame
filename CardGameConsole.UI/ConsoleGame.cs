e1079059b64b786f68703b29a3044f26136f07b5
Ryan <B9MBjDSXtnldpR3QvzuO9tVu@protonmail.ch>
2024-03-29 18:27:37 -0500

    more console work

--- CardGameConsole.UI/ConsoleGame.cs
+++ CardGameConsole.UI/ConsoleGame.cs
@@ -1,31 +1,72 @@
 ﻿using CardGame.BL.BlackJack;
+using static CardGame.BL.Models.Constants.BaseConstants;
 
 BlackjackGameManager blackjackGameManager;
+
 int playerCount;
 int startingBalance;
+int selectedGame;
+string gameChosen = string.Empty;
 bool gameReady = false;
 
 
 while (!gameReady)
 {
-    Console.WriteLine("Enter desired amount of opponents");
-
-    if (Int32.TryParse(Console.ReadLine(), out playerCount))
+    int i = 1;
+    Console.WriteLine("Select game type by its number...");
+    foreach (Enum gameType in Enum.GetValues(typeof(GameType)))
     {
-        Console.WriteLine("Enter desired starting balance.");
+        Console.WriteLine($"{i}. {gameType.ToString()}");
+        i++;
+    }
 
-        if (Int32.TryParse(Console.ReadLine(), out startingBalance))
+    if (Int32.TryParse(Console.ReadLine(), out selectedGame))
+    {
+        switch (selectedGame)
         {
-            Guid gameId = Guid.NewGuid();
-            blackjackGameManager = new BlackjackGameManager(gameId, playerCount, startingBalance);
-            gameReady = true;
-            PlayGame();
+            case 1:
+                gameChosen = "Blackjack";
+                break;
+            default: 
+                break;
+        }
+
+        if (!string.IsNullOrEmpty(gameChosen))
+        {
+            Console.WriteLine("Enter desired amount of opponents.");
+
+            if (Int32.TryParse(Console.ReadLine(), out playerCount))
+            {
+                Console.WriteLine("Enter desired starting balance.");
+
+                if (Int32.TryParse(Console.ReadLine(), out startingBalance))
+                {
+                    Guid gameId = Guid.NewGuid();
+
+                    switch (gameChosen)
+                    {
+                        case "Blackjack":
+                            blackjackGameManager = new BlackjackGameManager(gameId, playerCount, startingBalance);
+                            break;
+                        default: throw new Exception("There is no possible way you couldve made it here...get out of my program.");
+                    }
+
+                    gameReady = true;
+                    PlayGame();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid number.");
+            }
         }
     }
     else
     {
-        Console.WriteLine("Please enter a valid number");
+        Console.WriteLine("Please enter a valid game number");
     }
+
+    
 }
 
 static void PlayGame()
